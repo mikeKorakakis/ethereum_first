d48625e4beae8c8d3e34e719933fac5bf5ecef1a
mikeKorakakis <30752591+mikeKorakakis@users.noreply.github.com>
2022-09-30 17:41:30 +0300

    updated solc

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
@@ -10,9 +10,9 @@ let inbox;
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: env.bytecode.object,
       arguments: ["Hi there!"],
     })
     .send({ from: accounts[0], gas: "1000000" });
